cb94ab2533bab38f5232bb3e591f46aed21857d2
rival-ctrl <rivalnugraha5@gmail.com>
2023-11-11 21:20:47 +0700

    session 2 myanimelist

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import InputSearch from "./inputSearch";
 
 const Navbar = () => {
   return (
     <header>
       <div className="bg-indigo-500 flex md:flex-row flex-col justify-between p-4 gap-2">
-
-        <Link href="/" className="font-bold text-2xl text-white">MYANIMELIST</Link>
-        <input type="text" className="" placeholder="Search Anime .."/>
+        <Link href="/" className="font-bold text-2xl text-white">
+          MYANIMELIST
+        </Link>
+        <InputSearch />
       </div>
     </header>
   );
